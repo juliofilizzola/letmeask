80907b09dade8f388d0e8a04a8bb7e11b209f219
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 02:42:44 -0300

    iniciando pagina home

---
+++ letmeask/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import illustrationImg from '../../img/illustration.svg'
+function Home() {
+  return (
+    <div>
+      <aside> 
+        <img src={ illustrationImg } alt="Logo da pagina Home" />
+      </aside>
+    </div>
+  );
+}
+
+export default Home;
