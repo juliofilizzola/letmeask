d44c00a57580821b50d2cd16874c4daa06955545
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 18:33:58 -0300

    arrumando o import

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import illustrationImg from '../img/illustration.svg';
-import logoImg from '../../img/logo.svg';
-import googleImage from '../../img/google-icon.svg';
+import logoImg from '../img/logo.svg';
+import googleImage from '../img/google-icon.svg';
 
 function Home() {
   return (
