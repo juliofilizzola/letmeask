74678c48118f80f4ee637f19c01c718bf63cc08c
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 18:30:19 -0300

    importando e renderizando o Home

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './services/firebase';
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    
+    <Home />
   );
 }
 
