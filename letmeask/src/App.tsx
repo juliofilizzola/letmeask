14d147a8a51ed046142f9499b74f8694213c46d9
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-11 01:13:26 -0300

    criando função de verificação

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,14 +1,31 @@
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
-// import { Switch, Route } from 'react-router-dom'
+import { auth, firebase } from './services/firebase';
+
+export const AuthContext = React.createContext({} as any);
 
 function App() {
+  const [user, setUser] = React.useState();
+
+  const signWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then(result => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.')
+        }
+      }
+    });
+  }
   return (
     <Switch>
-      <Route exact path="/" component={ Home }/>
-      <Route exact path="/rooms/new" component={ NewRoom }/>
-
+      <AuthContext.Provider value={{ user, setUser }}>
+        <Route exact path="/" component={ Home }/>
+        <Route exact path="/rooms/new" component={ NewRoom }/>
+      </AuthContext.Provider>
     </Switch>
   );
 }
