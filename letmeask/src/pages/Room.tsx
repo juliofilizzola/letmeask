3f12e833cdfa81760d97477f2e8a6daede7d1967
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-13 07:57:00 -0300

    criando header, main e form para a pagina sala

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,4 +1,5 @@
 import logoImg from '../assets/img/logo.svg';
+import Button from '../components/Button';
 
 function Room() {
   return (
@@ -6,8 +7,24 @@ function Room() {
       <header>
         <div className="content">
           <img src={ logoImg } alt="logo" />
+          <div>code</div> 
         </div>
       </header>
+      
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala</h1>
+          <span></span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?"/>
+          <div className="form-footer">
+            <span> Para enviar uma perguntar, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar perguntar</Button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
