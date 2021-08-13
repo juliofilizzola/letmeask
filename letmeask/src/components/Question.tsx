78eb9d71b415e7be295a4060109f6a8d1569e207
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-13 17:04:01 -0300

    tipando question

--- letmeask/src/components/Question.tsx
+++ letmeask/src/components/Question.tsx
@@ -1,4 +1,12 @@
-function Question() {
+type QuestionsProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+}
+
+function Question({content , author}: QuestionsProps) {
   return (
     <div className="question">
       <p></p>
