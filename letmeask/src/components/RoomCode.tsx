7e49e565df0f59ae7bad83d89fcb1119e92b0013
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-13 08:56:38 -0300

    criando função copyRoomCodeToClipboard

--- letmeask/src/components/RoomCode.tsx
+++ letmeask/src/components/RoomCode.tsx
@@ -1,13 +1,23 @@
 import copyImg from '../assets/img/copy.svg';
 import '../styles/roomCode.scss';
 
-function RoomCode() {
+type RoomCodeProps = {
+  code: string,
+}
+
+function RoomCode(props: RoomCodeProps) {
+  const copyRoomCodeToClipboard = () => {
+    navigator.clipboard.writeText(props.code)
+  }
   return (
-    <button className="room-code">
+    <button
+      className="room-code"
+      onClick={ copyRoomCodeToClipboard }
+    >
       <div>
         <img src={ copyImg } alt="Copy room code" />
       </div>
-      <span>Sala </span>
+      <span>Sala #{props.code}</span>
     </button>
   );
 }
