6b8683045843c01d7fbf90a2e515f4616b5d791e
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-15 08:54:05 -0300

    importando icone de deletar no admin room

--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -6,6 +6,7 @@ import Question from '../components/Question';
 import RoomCode from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import useRoom from '../hooks/useRoom';
+import deletImg from '../assets/img/delete.svg';
 import { database } from '../services/firebase';
 import '../styles/room.scss';
 
