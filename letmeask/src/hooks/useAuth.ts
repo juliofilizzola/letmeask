a4492250d4dbf9d90240e7657dd4301d1e4a6993
Filizzola <juliofilizzola@hotmail.com>
2022-04-29 08:30:49 -0300

    ⚰️ : deletando codigo para primeira refect

--- letmeask/src/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { AuthContext } from '../context/AuthContext';
-
-export const useAuth = () => {
-  const value = React.useContext(AuthContext);
-  return value;
-}
